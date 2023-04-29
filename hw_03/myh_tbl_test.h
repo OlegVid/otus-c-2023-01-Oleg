6d06634a2549801a81a3bd7decb01bb4da6a2af2
Oleg Videnin <soapmaker@yandex.ru>
2023-04-29 04:26:05 -0700

    spare files deleted

--- hw_03/myh_tbl_test.h
+++
@@ -1,6 +0,0 @@
-#define INJECT_ERROR 0
-#define INJECT_ERROR_ITEM_NO 123
-#define START_TABLE_CAPACITY 2000
-#define TEST_ITEM_COUNT      500
-
-int test_myh_tbl_unit(void); //return 0 if test success
